8f3dbede831a590ad59249078e563af9cf7c2d96
unknown <yngkri2@gmail.com>
2019-07-08 14:31:08 +0200

    m

--- src/Components/resources/img.js
+++
@@ -1,7 +0,0 @@
-import img1 from './anntph.jpg'
-import img2 from './anntph.jpg'
-
-export default {
-  img1,
-  img2,
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Root from "./Components/Root";
-import store from './store';
-import './Components/styles/index.scss';
-
-/** Index file with Root component **/
-ReactDOM.render(<Root store={store} />, document.getElementById('root'));
